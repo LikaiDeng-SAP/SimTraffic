87051928b173d7bd9211202399234346d6fd740c
Likai Deng <denglikai@live.com>
2017-12-08 17:02:58 +0800

    matplot-1

--- Car.py
+++ Car.py
@@ -1,17 +1,20 @@
 class Car(object):
-
     def __init__(self,initialDistance,initialSpeed,name):
         self.distance = initialDistance
         self.acc = 0
         self.speed = initialSpeed
         self.name = name
+        self.self_distance_result = []
+        self.self_speed_result = []
 
     def change_speed(self,acc):
         self.speed += acc
+        self.self_speed_result.append(self.speed)
 
     def drive_forward(self,time,acc):
         if acc == 0 :
             self.distance += self.speed*time
+            self.self_distance_result.append(int(self.distance))
         elif acc != 0 :
             self.distance += self.speed*time + acc*time*time/2
-        #print(self.name +': ' + str(self.distance))
+            self.self_distance_result.append(int(self.distance))
--- tools.py
+++ tools.py
@@ -1,3 +1,4 @@
+import random
 def get_delta_distance(Hcar,Tcar):
     return Hcar.distance - Tcar.distance
 
@@ -7,9 +8,9 @@ def reaction_of_change(car):
     Hacc = car.Hcar.acc
     delta_distance = get_delta_distance(car.Hcar,car)
     if Hacc >= 0:
-        car.acc = Hacc * (delta_distance/50)
+        car.acc = Hacc * (delta_distance/50)*random.uniform(-0.01, 0.01)
     elif Hacc< 0:
-        car.acc = Hacc * 1.5
+        car.acc = Hacc * 1.5*random.uniform(-0.01, 0.01)
 
 
 def print_distance_result(cars):
@@ -22,5 +23,7 @@ def cars_driving_forward(cars,time):
     for car in cars :
         if car.acc == 0 :
             car.distance += car.speed*time
+            car.distance_result.append(car.distance)
         elif car.acc != 0 :
             car.distance += car.speed*time + car.acc*time*time/2
+            #car.distance_result.append(car.distance)
